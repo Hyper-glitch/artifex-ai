2b9b0ffa86b60f2d99054a71f254f978af9f2782
Смирнов Роман Олегович <romasmirnov@samokat.ru>
2025-06-02 16:00:56 +0300

    feat: add SignUpStatus enum

--- api/src/enums.py
+++ api/src/enums.py
@@ -1,14 +1,28 @@
-from enum import Enum
+from enum import StrEnum
+
+from starlette import status
 
 
-class UserRole(str, Enum):
+class UserRole(StrEnum):
     USER = "user"
     ADMIN = "admin"
 
 
-class TaskStatus(str, Enum):
+class TaskStatus(StrEnum):
     NEW = "new"
     PENDING = "pending"
     PROCESSING = "processing"
     COMPLETED = "completed"
     FAILED = "failed"
+
+
+class SignUpStatus(StrEnum):
+    CREATED = "created"
+    ALREADY_EXISTS = "already_exists"
+
+    @property
+    def http_status(self) -> int:
+        return {
+            self.ALREADY_EXISTS: status.HTTP_409_CONFLICT,
+            self.CREATED: status.HTTP_201_CREATED,
+        }.get(self, status.HTTP_500_INTERNAL_SERVER_ERROR)
