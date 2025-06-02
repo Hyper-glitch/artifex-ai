598b2883dd952f65a4418d438584754c2aabfa14
Смирнов Роман Олегович <romasmirnov@samokat.ru>
2025-06-02 16:02:42 +0300

    feat: add user model

---
+++ api/src/models/user.py
@@ -0,0 +1,19 @@
+from database import Base
+from enums import UserRole
+from sqlalchemy import Column
+from sqlalchemy import Enum as SQLAlchemyEnum
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import relationship
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=True, unique=True)
+    first_name = Column(String, nullable=False, unique=True)
+    last_name = Column(String, nullable=True, unique=True)
+    language_code = Column(String, nullable=True, unique=True)
+    role: UserRole = Column(SQLAlchemyEnum(UserRole), default=UserRole.USER)
+
+    tasks = relationship("MLTask", back_populates="user")
