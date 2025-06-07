d51ba0487ebb7256a8b35e13403b47467c03f476
Смирнов Роман Олегович <graff049@gmail.com>
2025-06-07 13:59:13 +0300

    feat: improve worker

--- worker/src/clients/atrifex.py
+++ worker/src/clients/atrifex.py
@@ -2,18 +2,15 @@ from urllib.parse import urljoin
 
 from aiohttp import ClientSession
 
-from enums import TaskStatus
+from dto import UpdateTaskRequest
 
 
 class AsyncApiClient:
     def __init__(self, base_url: str, token: str) -> None:
         self._session = self._create_session(token, base_url)
 
-    async def update_task(self, task_id: str, status: TaskStatus, detail: str) -> None:
-        async with self._session.post(
-            "/worker/tasks/update/",
-            json={"task_id": str(task_id), "status": status, "detail": detail},
-        ) as resp:
+    async def update_task(self, dto: UpdateTaskRequest) -> None:
+        async with self._session.post("tasks/update/", json=dto.model_dump()) as resp:
             resp.raise_for_status()
 
     @staticmethod
--- worker/src/service.py
+++ worker/src/service.py
@@ -5,7 +5,7 @@ import numpy as np
 from clients.atrifex import AsyncApiClient
 from clients.tg_bot import TgBotClient
 from clients.triton import AsyncTritonClient
-from dto import RMQMessage
+from dto import RMQMessage, UpdateTaskRequest
 from enums import TaskStatus
 
 
@@ -19,33 +19,46 @@ class GenAIProcessingService:
 
     async def process_task(self, msg: RMQMessage) -> None:
         logging.info(f"Starting to process genAI task {msg.task_id}...")
-        await self._safe_update_status(msg.task_id, TaskStatus.PROCESSING)
-
+        await self._safe_update_status(
+            dto=UpdateTaskRequest(
+                task_id=msg.task_id,
+                status=TaskStatus.PROCESSING,
+            )
+        )
         try:
             image_np = await self._triton_client.infer(msg)
         except Exception as exc:
             await self._safe_update_status(
-                msg.task_id, TaskStatus.FAILED, detail=f"Inference failed: {exc}"
+                dto=UpdateTaskRequest(
+                    task_id=msg.task_id,
+                    status=TaskStatus.FAILED,
+                    logs=f"Inference failed: {exc}",
+                )
             )
-            logging.error(f"ML inference failed for task {msg.task_id}: {exc}")
+            await self._tg_bot.send_error_msg(msg.chat_id)
+            logging.error(f"AI inference failed for task {msg.task_id}: {exc}")
             return
 
         await self._safe_update_status(
-            msg.task_id, TaskStatus.COMPLETED, detail="Inference successful"
+            dto=UpdateTaskRequest(
+                task_id=msg.task_id,
+                status=TaskStatus.COMPLETED,
+                logs="Inference successful",
+            )
         )
-        await self._safe_send_photo(msg.chat_id, image_np, caption="Вот ваше изображение")
+        await self._safe_send_photo(msg.chat_id, image_np, task_id=msg.task_id)
         logging.info(f"GenAI task - {msg.task_id} - successfully processed.")
 
-    async def _safe_update_status(
-        self, task_id: str, status: TaskStatus, detail: str | None = None
+    async def _safe_update_status(self, dto: UpdateTaskRequest) -> None:
+        try:
+            await self._client.update_task(dto)
+        except Exception as exc:
+            logging.error(f"Failed to update status {dto.status} for task {dto.task_id}: {exc}")
+
+    async def _safe_send_photo(
+        self, chat_id: int | str, image_np: np.ndarray, task_id: str
     ) -> None:
         try:
-            await self._client.update_task(task_id=task_id, status=status, detail=detail)
-        except Exception as exc:
-            logging.error(f"Failed to update status {status} for task {task_id}: {exc}")
-
-    async def _safe_send_photo(self, chat_id: int | str, image_np: np.ndarray, caption: str) -> None:
-        try:
-            await self._tg_bot.send_photo(chat_id=chat_id, image_np=image_np, caption=caption)
+            await self._tg_bot.send_photo(chat_id=chat_id, image_np=image_np, task_id=task_id)
         except Exception as exc:
             logging.error(f"Failed to send photo to chat {chat_id}: {exc}")
