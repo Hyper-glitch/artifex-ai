3604e30635cc939131d0261e8e17c786901810c4
Смирнов Роман Олегович <graff049@gmail.com>
2025-06-07 13:55:31 +0300

    feat: add dialog msgs

--- tg-bot/src/dialogs.py
+++ tg-bot/src/dialogs.py
@@ -1,20 +1,21 @@
 from http import HTTPStatus
 
-WELCOME_MESSAGE = "Добро пожаловать в сервис генерации контента ArtifexAI!\nДля начала работы необходимо зарегистрироваться в системе."
 SUCCESS_SIGN_UP = "Вы успешно зарегистрированы в системе 🎉"
 ALREADY_SIGN_UP = "Вы уже зарегистрированны в системе"
 START_MESSAGE = (
     "👋 Привет! Я бот Artifex-AI.\n\n"
     "Опиши, что ты хочешь сгенерировать, и я создам визуал в фирменном стиле."
 )
+AWAITING_PROMPT_MESSAGE = "Пожалуйста, опишите изображение, которое вы хотите создать (in English):"
 PROCESSING_MESSAGE = "⏳ Добавил в очередь на генерацию.\nБудет готово примерно через 10 секунд."
 ACCEPTED_MESSAGE = "Супер! Изображение принято ✅"
 RATE_PROMPT_MESSAGE = "Пожалуйста, оцените сгенерированное изображение по шкале от 1 до 5 ⭐"
 RATE_THANKS_MESSAGE = "Спасибо за вашу оценку! 🙏"
 ASK_NEW_PROMPT_MESSAGE = "Если нужно новое изображение — просто напиши описание."
-REGENERATING_MESSAGE = "Перегенерирую 🔁"
+
 ERROR_MESSAGE = "⚠️ Упс, произошла ошибка. Попробуйте еще раз."
 NOT_AUTH_MESSAGE = "❌ Вам необходимо зарегистрироваться в системе."
+ERROR_TASK_MESSAGE = "❌ Error creating task."
 
 STATUS_MESSAGES = {
     HTTPStatus.CREATED: SUCCESS_SIGN_UP,
