5af22c9e480f3be4e3d9c2230a0b07ecda2f70e5
Смирнов Роман Олегович <graff049@gmail.com>
2025-06-08 15:33:47 +0300

    feat: assert producer in test

--- api/tests/integration/test_task.py
+++ api/tests/integration/test_task.py
@@ -1,3 +1,5 @@
+from unittest.mock import AsyncMock
+
 import pytest
 from fastapi import status
 from httpx import AsyncClient
@@ -10,16 +12,32 @@ class TestMLTaskService:
     _update_url = "tasks/update/"
 
     @pytest.mark.asyncio
-    async def test_create_task_success(self, async_client: AsyncClient, new_task_dto: AITaskRequest) -> None:
+    async def test_create_task_success(
+        self,
+        async_client: AsyncClient,
+        new_task_dto: AITaskRequest,
+        mock_producer: AsyncMock,
+    ) -> None:
         """Тест успешного создания новой задачи на генерацию."""
         response = await async_client.post(
             self._create_url, json=new_task_dto.model_dump(mode="json")
         )
+        mock_producer.publish.assert_called_once()
+        called_args = mock_producer.publish.call_args[1]
+
+        assert called_args["queue_name"] == "gen-ai-tasks"
+        assert called_args["message"].prompt == new_task_dto.prompt
+        assert called_args["message"].chat_id == 1
+
         assert response.status_code == status.HTTP_200_OK
         assert response.json() == {"success": True}
 
     @pytest.mark.asyncio
-    async def test_update_task_success(self, async_client, update_task_request: UpdateTaskRequest) -> None:
+    async def test_update_task_success(
+        self,
+        async_client: AsyncClient,
+        update_task_request: UpdateTaskRequest,
+    ) -> None:
         """Тест на успешное обновление задачи."""
         response = await async_client.post(
             self._update_url, json=update_task_request.model_dump()
