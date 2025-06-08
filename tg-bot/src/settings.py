c62e05a5e0c383f165f39df3be572b2fc04741fa
Смирнов Роман Олегович <graff049@gmail.com>
2025-06-08 14:06:36 +0300

    feat: remove extra

--- tg-bot/src/settings.py
+++ tg-bot/src/settings.py
@@ -6,7 +6,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=Path(__file__).parent.parent / ".env")
 
-    DEBUG: bool = False
     LOG_LEVEL: str = "INFO"
     TELEGRAM_BOT_TOKEN: str
 
